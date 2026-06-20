c2790c2b027d7739aa8faee1c4276f0e57465520
hruthika18 <rontalahruthika@gmail.com>
2026-06-20 09:56:00 +0530

    Toogle Complete

---
+++ frontend/js/toggle.js
@@ -0,0 +1,31 @@
+var users = [
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "image": "../images/john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "../images/jane.png"
+    }
+]
+
+var curId = 0;
+function toggle() {
+    // curId = (curId + 1) % users.length;
+    curId = (curId + 1) % 2;
+
+    // toggle the rendered user details
+
+    // image
+    var user = users[curId];
+    document.getElementById("user-img").src = user.image;
+
+    // name
+    document.getElementById("user-name").innerText = user.name;
+
+    // gender
+    document.getElementById("user-gender").textContent = user.gender;
+}
+
